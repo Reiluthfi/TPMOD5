c41d4327d544880c317a7d02b4cbd82ca453d001
Reiluthfi <164453246+Reiluthfi@users.noreply.github.com>
2025-03-16 11:55:18 +0700

    menambahkan class DataGeneric

--- TPMOD5/DataGeneric.cs
+++ TPMOD5/DataGeneric.cs
@@ -1,4 +1,13 @@
-﻿class DataGeneric<T>
+﻿static void Main()
+{
+    // Mengisi data dengan NIM (contoh: 123456)
+    DataGeneric<int> dataNIM = new DataGeneric<int>(112);
+
+    // Memanggil method PrintData()
+    dataNIM.PrintData();
+}
+
+class DataGeneric<T>
 {
     private T data; // Properti data bertipe generic T
 

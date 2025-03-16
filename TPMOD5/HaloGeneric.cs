98e174cd1c26aa35910181dc951ed1f75b2d67f7
Reiluthfi <164453246+Reiluthfi@users.noreply.github.com>
2025-03-16 11:37:25 +0700

    menambahkan class HaloGeneric dengan method SapaUser

---
+++ TPMOD5/HaloGeneric.cs
@@ -0,0 +1,8 @@
+﻿public class HaloGeneric
+{
+    // Method dengan Generic Parameter  
+    public static void SapaUser<T>(T input)
+    {
+        Console.WriteLine("Halo user " + input);
+    }
+}
